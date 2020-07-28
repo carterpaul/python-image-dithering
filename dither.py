6ab7dde215e664bc8fe0212b316af7c4f02c7b1d
Carter Sifferman <cpsiff@gmail.com>
2020-07-28 17:15:49 -0500

    Documentation and clean-up

--- dither.py
+++ dither.py
@@ -1,10 +1,16 @@
 from PIL import Image
 import random
 
-OUTPUT_WIDTH=256
-DITHER_SCALE=3
+"""
+Generate random patterns blocks of for use with pattern dithering
 
-def generate_blocks(s):
+Args:
+    s: The width (and height) of pattern blocks to be generated
+
+Returns:
+    A 2D array of pattern blocks. The array will have (s*s)+1 elements corresponding to each shade
+"""
+def generate_random_pattern_blocks(s):
     blocks = []
     for i in range((s*s)+1):
         indeces = random.sample(range(s*s), i)
@@ -17,32 +23,47 @@ def generate_blocks(s):
         blocks.append(block)
     return blocks
 
-BLOCKS = generate_blocks(DITHER_SCALE)
-print(BLOCKS)
+"""
+Put a square block of pixels starting at a given top-left corner position
 
-def put_pixels(img, x, y, s, values):
+Args:
+    img: PIL Image object for the pixels to be put in
+    x: x-coordinate of top-left corner of block
+    y: y-coordinate of top-left corner of block
+    s: Width of the block
+    pixels: Array of pixels to be placed (should contain s*s elements)
+"""
+def put_pixels(img, x, y, s, pixels):
     for i in range(s):
         for j in range(s):
-            img.putpixel((x*s+i, y*s+j), values[i*s+j])
+            img.putpixel((x*s+i, y*s+j), pixels[i*s+j])
 
-img = Image.open("input_img.jpeg")
+def main():
+    OUTPUT_WIDTH=256
+    DITHER_SCALE=2
+    BLOCKS = generate_random_pattern_blocks(DITHER_SCALE)
 
-scale = OUTPUT_WIDTH/img.width
-img = img.resize((OUTPUT_WIDTH, int(img.height*scale)))
+    img = Image.open("input_img.jpeg")
 
-dithered_img = Image.new('1', (img.width*DITHER_SCALE, img.height*DITHER_SCALE))
+    scale = OUTPUT_WIDTH/img.width
+    img = img.resize((OUTPUT_WIDTH, int(img.height*scale)))
 
-for x in range(img.width):
-    for y in range(img.height):
-        shade = sum(img.getpixel((x,y)))
-        band_size = 766 / ((DITHER_SCALE*DITHER_SCALE)+1)
-        i=0
-        while i < DITHER_SCALE*DITHER_SCALE+1:
-            if shade < band_size*(i+1):
-                put_pixels(dithered_img, x, y, DITHER_SCALE, BLOCKS[i])
-                i = 999
-            else:
-                i = i+1
+    dithered_img = Image.new('1', (img.width*DITHER_SCALE, img.height*DITHER_SCALE))
 
-img.show()
-dithered_img.show()
+    for x in range(img.width):
+        for y in range(img.height):
+            shade = sum(img.getpixel((x,y)))
+            band_size = 766 / ((DITHER_SCALE*DITHER_SCALE)+1)
+            i=0
+            while i < DITHER_SCALE*DITHER_SCALE+1:
+                if shade < band_size*(i+1):
+                    put_pixels(dithered_img, x, y, DITHER_SCALE, BLOCKS[i])
+                    i = 999
+                else:
+                    i = i+1
+
+    img.show()
+    dithered_img.show()
+
+if __name__ == "__main__":
+    main()
